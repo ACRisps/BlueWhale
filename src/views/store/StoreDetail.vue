4b8092a9fab40bc78c493b2ffd7a83845ba2ae8e
acrisps <221250020@smail.nju.edu.cn>
2024-03-29 21:40:41 +0800

    refactor:“商店详情”界面调整显示效果

--- src/views/store/StoreDetail.vue
+++ src/views/store/StoreDetail.vue
@@ -25,20 +25,20 @@ onMounted(() => {
     <!--希望把商店详情的一部分内容放在这个侧边栏里，你要真不想放也没事-->
     <el-aside width="25%" class="page-aside">
       <el-row justify="center" align="middle">
-        <el-col :span="8">
+        <el-col :span="4">
           <router-link :to="'/allStore'" v-slot="{navigate}" class="no-link">
             <el-icon class="arrow" @click="navigate">
               <ArrowLeft/>
             </el-icon>
           </router-link>
         </el-col>
-        <el-col :span="8">
+        <el-col :span="16">
           <div class="storeTitle">{{ storeDetail.storeName }}</div>
         </el-col>
-        <el-col :span="8"></el-col>
+        <el-col :span="4"></el-col>
       </el-row>
       <el-row justify="center">
-        <el-image class="storeImg" :src="storeDetail.imgURL" :fit="'contain'"/>
+        <el-image class="storeImg" :src="storeDetail.imgURL" :fit="'fill'"/>
       </el-row>
       <el-row>
         <el-text class="storeInfo">{{ storeDetail.description }}</el-text>
@@ -82,6 +82,7 @@ onMounted(() => {
   font-size: 20px;
   margin-top: 15px;
   margin-bottom: 15px;
+  text-align: center;
 }
 
 .storeImg {
@@ -97,5 +98,9 @@ onMounted(() => {
 
 .storeInfo {
   margin: 20px;
+}
+
+.card{
+  margin: 5px;
 }
 </style>
