7d218a6aeed2909ccbf5ddb27287369f2917f1d6
acrisps <221250020@smail.nju.edu.cn>
2024-04-30 19:06:30 +0800

    feat:处理优惠券使用后的显示

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,6 +1,6 @@
 export const progressColors = [
-    { color: '#f56c6c', percentage: 20 },
-    { color: '#e6a23c', percentage: 40 },
-    { color: '#5cb87a', percentage: 60 },
+    { color: '#f56c6c', percentage: 21 },
+    { color: '#e6a23c', percentage: 41 },
+    { color: '#5cb87a', percentage: 61 },
     { color: '#1989fa', percentage: 100 },
 ]
